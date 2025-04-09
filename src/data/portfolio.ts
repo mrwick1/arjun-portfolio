36798b493e6bb1250f9ab3b8c975800f777218d1
Arjun KR <arjunkrishnaraj123@gmail.com>
2025-04-09 20:33:34 +0530

    commented out portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -8,42 +8,42 @@ export interface Portfolio {
 }
 
 export const portfolioData: Portfolio[] = [
-  {
-    title: "Brain Wave",
-    description:
-      "A modern and fully responsive website with a focus on excellent UI/UX.",
-    technologies: ["React", "Tailwind CSS"],
-    projectUrl: "https://brainwave-blue.vercel.app/",
-    imageUrl:
-      "/assets/images/carrent.webp",
-    codeUrl: "https://github.com/mrwick1/brainwave",
-  },
-  {
-    title: "Hoobank",
-    description:
-      "A stunning and fully responsive website designed for a financial client.",
-    technologies: ["React", "Tailwind CSS"],
-    projectUrl: "https://hoobank-sepia.vercel.app/",
-    imageUrl:
-      "/assets/images/jobit.webp",
-    codeUrl: "https://github.com/mrwick1/hoobank",
-  },
-  {
-    title: "Open AI",
-    description: "A clean and fully responsive website.",
-    technologies: ["React", "CSS"],
-    projectUrl: "https://open-ai-dun.vercel.app/",
-    imageUrl:
-      "/assets/images/tripguide.webp",
-    codeUrl: "https://github.com/mrwick1/open-ai",
-  },
-  {
-    title: "Glisten AI",
-    description: "A modern Next.js website with engaging animations.",
-    technologies: ["Next.js", "Tailwind CSS", "GSAP"],
-    projectUrl: "https://glisten-ai-wheat.vercel.app/",
-    imageUrl:
-      "/assets/images/glisten.webp", 
-    codeUrl: "https://github.com/mrwick1/glisten-app",
-  },
+  // {
+  //   title: "Brain Wave",
+  //   description:
+  //     "A modern and fully responsive website with a focus on excellent UI/UX.",
+  //   technologies: ["React", "Tailwind CSS"],
+  //   projectUrl: "https://brainwave-blue.vercel.app/",
+  //   imageUrl:
+  //     "/assets/images/carrent.webp",
+  //   codeUrl: "https://github.com/mrwick1/brainwave",
+  // },
+  // {
+  //   title: "Hoobank",
+  //   description:
+  //     "A stunning and fully responsive website designed for a financial client.",
+  //   technologies: ["React", "Tailwind CSS"],
+  //   projectUrl: "https://hoobank-sepia.vercel.app/",
+  //   imageUrl:
+  //     "/assets/images/jobit.webp",
+  //   codeUrl: "https://github.com/mrwick1/hoobank",
+  // },
+  // {
+  //   title: "Open AI",
+  //   description: "A clean and fully responsive website.",
+  //   technologies: ["React", "CSS"],
+  //   projectUrl: "https://open-ai-dun.vercel.app/",
+  //   imageUrl:
+  //     "/assets/images/tripguide.webp",
+  //   codeUrl: "https://github.com/mrwick1/open-ai",
+  // },
+  // {
+  //   title: "Glisten AI",
+  //   description: "A modern Next.js website with engaging animations.",
+  //   technologies: ["Next.js", "Tailwind CSS", "GSAP"],
+  //   projectUrl: "https://glisten-ai-wheat.vercel.app/",
+  //   imageUrl:
+  //     "/assets/images/glisten.webp", 
+  //   codeUrl: "https://github.com/mrwick1/glisten-app",
+  // },
 ];
